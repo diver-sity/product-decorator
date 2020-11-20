1424194ee818eff55129ee9382a1579b03315b2d
Lee Zhao <lee.simon3@hotmail.com>
2020-11-20 17:56:42 +1100

    test fixtures

---
+++ src/fixtures/Products.ts
@@ -0,0 +1,13 @@
+import Product from "src/models/Product";
+
+export const socks: Product = {
+    video_count: 2,
+    price: 2.5,
+    markdown_price: 2,
+    special_price: 1.5,
+    returnable: false,
+    final_sale: false,
+    final_price: null,
+    sku: "ksk53-skd82-gg232-jd8s2",
+    name: "soccer socks"
+}
