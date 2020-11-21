32e143c0b19fcad3bcdb200c2238d3685a680dd4
Lee Zhao <lee.simon3@hotmail.com>
2020-11-21 15:03:19 +1100

    more tests

--- src/services/ZeroVideoProductLoader.spec.ts
+++ src/services/ZeroVideoProductLoader.spec.ts
@@ -5,40 +5,51 @@ import * as fs from "fs";
 describe('VideoLinkDecorator', () => {
 
   describe('#saveProducts', () => {
-    it('saves a stringified array of products, removing the brackets', () => {
-      let pathToClear;
-      expect.assertions(1);
-      return saveProducts([socks], 1).then(path => {
-        const result = fs.readFileSync(path);
-        pathToClear = path;
-        expect(JSON.parse(result.toString())).toEqual(socks);
-      }).finally(() => {
-        if (pathToClear) {
-          try {
-            fs.unlinkSync(pathToClear);
-          } catch (error) {
-            console.error(`failed to remove ${pathToClear}, ${error}`)
-          }
-        }
-      })
-    });
-
-    describe('if write fails', () => {
-      let writeFile: jest.SpyInstance;
-
-      beforeEach(() => {
-        jest.resetAllMocks();
-        writeFile = jest.spyOn(fs.promises, "writeFile");
-      })
-
-      it('passes the error to the caller', () => {
-
-        writeFile.mockRejectedValue(new Error("EACCES"));
+    describe('if there is no product', () => {
+      it('does nothing', () => {
         expect.assertions(1);
-        return saveProducts([socks], 1).catch((error: Error) => {
-          expect(error.message).toEqual("EACCES");
+        return saveProducts([], 1).then((result) => {
+          expect(result).toBeUndefined();
         })
       });
-    })
+    });
+
+    describe('if there is a product or more', () => {
+      it('saves a stringified array of products, removing the brackets', () => {
+        let pathToClear;
+        expect.assertions(1);
+        return saveProducts([socks], 1).then(path => {
+          const result = fs.readFileSync(path);
+          pathToClear = path;
+          expect(JSON.parse(result.toString())).toEqual(socks);
+        }).finally(() => {
+          if (pathToClear) {
+            try {
+              fs.unlinkSync(pathToClear);
+            } catch (error) {
+              console.error(`failed to remove ${pathToClear}, ${error}`)
+            }
+          }
+        })
+      });
+
+      describe('if write fails', () => {
+        let writeFile: jest.SpyInstance;
+
+        beforeEach(() => {
+          jest.resetAllMocks();
+          writeFile = jest.spyOn(fs.promises, "writeFile");
+        })
+
+        it('passes the error to the caller', () => {
+
+          writeFile.mockRejectedValue(new Error("EACCES"));
+          expect.assertions(1);
+          return saveProducts([socks], 1).catch((error: Error) => {
+            expect(error.message).toEqual("EACCES");
+          })
+        });
+      })
+    });
   });
 });
--- src/services/ZeroVideoProductLoader.ts
+++ src/services/ZeroVideoProductLoader.ts
@@ -2,7 +2,11 @@ import * as fs from "fs";
 import { v4 as getId } from "uuid";
 import Product from "src/models/Product";
 
-const saveProducts = (products: Array<Product>, pageNumber: number) => {
+const saveProducts = (products: Array<Product>, pageNumber: number): Promise<void | string> => {
+    if (products.length === 0) {
+        return Promise.resolve();
+    }
+
     const path = `/tmp/product-decorator-${pageNumber}-${getId()}.txt`;
     const output = JSON.stringify(products);
     return fs.promises.writeFile(path, output.substring(1, output.length - 1))
