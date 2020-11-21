276eaec3545c95a1daa5b6517369a19b50cfcbf9
Lee Zhao <lee.simon3@hotmail.com>
2020-11-21 17:35:12 +1100

    access the data source and process data

--- src/services/RestAPIExtractor.spec.ts
+++ src/services/RestAPIExtractor.spec.ts
@@ -1,11 +1,21 @@
 import axios from "../config/axiosInstance";
 import fetchFromSource from "./RestAPIExtractor";
+import * as ZeroVideoProductLoader from "./ZeroVideoProductLoader";
+import * as VideoLinkedProductLoader from "./VideoLinkedProductLoader";
+import * as VideoLinkDecorator from "./VideoLinkDecorator";
+import { productWithNoVideo, productWithVideo, productWithVideoURL } from "../fixtures/Products";
 
 describe('RestAPIExtractor', () => {
   let get: jest.SpyInstance;
+  let saveProducts: jest.SpyInstance;
+  let saveLinkedProducts: jest.SpyInstance;
+  let addVideoURL: jest.SpyInstance;
 
   beforeEach(() => {
     get = jest.spyOn(axios, "get");
+    saveProducts = jest.spyOn(ZeroVideoProductLoader, "default");
+    saveLinkedProducts = jest.spyOn(VideoLinkedProductLoader, "default");
+    addVideoURL = jest.spyOn(VideoLinkDecorator, "default");;
   });
 
   describe('#fetchFromSource', () => {
@@ -34,6 +44,28 @@ describe('RestAPIExtractor', () => {
             expect(get).toBeCalledTimes(2);
           })
         });
+      });
+    });
+
+    describe('if the page has products', () => {
+      it('separates the products into two sets, decorates one set and saves products into files', () => {
+        get.mockResolvedValueOnce({
+          data: {
+            _embedded: {
+              product: [productWithNoVideo, productWithVideo]
+            }
+          }
+        });
+
+        saveProducts.mockResolvedValue("/path/to/temp/file");
+        saveLinkedProducts.mockResolvedValue(null);
+        addVideoURL.mockResolvedValue(productWithVideoURL);
+        expect.assertions(3);
+        return fetchFromSource("http://localhost/1").then(() => {
+          expect(saveProducts).toBeCalledTimes(1);
+          expect(saveLinkedProducts).toBeCalledTimes(1);
+          expect(addVideoURL).toBeCalledTimes(1);
+        })
       });
     });
 
--- src/services/RestAPIExtractor.ts
+++ src/services/RestAPIExtractor.ts
@@ -1,17 +1,30 @@
 import { isEmpty } from "lodash";
 import axios from "../config/axiosInstance";
 import ProductPagePayload from "src/dto/ProductPagePayload";
+import saveProducts from "./ZeroVideoProductLoader";
+import saveLinkedProducts from "./VideoLinkedProductLoader";
+import addVideoURL from "./VideoLinkDecorator";
 
 const fetchFromSource = async (firstPageURL: string) => {
     const remainingPage = [firstPageURL];
+    const paths = [];
 
     try {
         while (remainingPage.length > 0) {
             const response = await axios.get(remainingPage.pop());
 
             const data = response.data as ProductPagePayload;
-            if (!isEmpty(data) && !isEmpty(data._links) && !isEmpty(data._links.next)) {
-                remainingPage.push(data._links.next.href);
+            if (!isEmpty(data)) {
+                let isLast = false;
+                if (!isEmpty(data._links)) {
+                    if (!isEmpty(data._links.next)) {
+                        remainingPage.push(data._links.next.href);
+                    } else {
+                        isLast = true;
+                    }
+                }
+
+                await processPage(data, isLast, paths);
             }
         }
     } catch (error) {
@@ -21,5 +34,22 @@ const fetchFromSource = async (firstPageURL: string) => {
 
     return Promise.resolve();
 };
+
+async function processPage(data: ProductPagePayload, isLast: boolean, paths: Array<string>) {
+    const page = data.page;
+    if (!isEmpty(data._embedded) && !isEmpty(data._embedded.product)) {
+        const products = data._embedded.product;
+        const withoutVideos = products.filter(p => p.video_count == 0);
+        await saveProducts(withoutVideos, page).then((path) => {
+            if (path) {
+                paths.push(path);
+            }
+        });
+
+        const withVideos = products.filter(p => p.video_count > 0);
+        const withVideosOps = withVideos.map(p => addVideoURL(p))
+        await Promise.all(withVideosOps).then(decoProducts => saveLinkedProducts(decoProducts, page, isLast));
+    }
+}
 
 export default fetchFromSource;
