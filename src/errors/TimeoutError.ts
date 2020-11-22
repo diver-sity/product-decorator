55ae8a13a5856f718d5935cce67c4e163dbb1f2a
Lee Zhao <lee.simon3@hotmail.com>
2020-11-22 21:38:04 +1100

    errors

---
+++ src/errors/TimeoutError.ts
@@ -0,0 +1,15 @@
+class TimeoutError extends Error {
+    private _error: Error;
+    constructor(message?: string, error?: Error) {
+        super(message);
+        this._error = error;
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    get error() {
+        return this._error;
+    }
+
+}
+
+export default TimeoutError;
