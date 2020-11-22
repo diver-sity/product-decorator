d6c46504acaaffe6b0e2cf7251e404f9f0905236
Lee Zhao <lee.simon3@hotmail.com>
2020-11-22 21:55:33 +1100

    integrated the timeout mechanism

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from "fs";
-import fetchFromSource from "./services/RestAPIExtractor";
+import fetchFromSource, { redoLog } from "./services/RestAPIExtractor";
 import assembleSmallFiles from './services/InterimFileAssembler';
-import { path } from './services/VideoLinkedProductLoader';
 import logger from "./config/Logger";
-import cleanUp from './services/Cleaner';
+import cleanUp, { deleteExistingOutFile } from './services/Cleaner';
+import TimeoutError from './errors/TimeoutError';
+
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.close();
 
+  let paths = [];
   try {
-    if (fs.existsSync(path)) {
-      fs.unlinkSync(path);
+    paths = await fs.promises.readFile(redoLog)
+      .then((buffer) => buffer.toString())
+      .catch(error => {
+        console.info(`no redoLog, starting from Page 1: `, JSON.stringify(error, undefined, 2));
+        deleteExistingOutFile();
+      }).then((url) => {
+        return fetchFromSource(url || "https://eve.theiconic.com.au/catalog/products?page=1&page_size=1000&sort=activated_at");
+      })
+  } catch (error) {
+    logger.error(`${error.message}, ${JSON.stringify(error)}`);
+    if (error instanceof TimeoutError) {
+      process.exit(1);
     }
-  } catch (error) {
-    logger.error(`failed to clean up before starting, ${JSON.stringify(error)}`);
-    process.exit(2)
-  }
-
-  let paths;
-  try {
-    paths = await fetchFromSource("https://eve.theiconic.com.au/catalog/products?page=1&page_size=1000&sort=activated_at");
-  } catch (error) {
-    logger.error(`failed to fetch data, ${JSON.stringify(error)}`);
   }
 
   try {
@@ -33,7 +35,12 @@ async function bootstrap() {
     logger.error(`failed to assemble temporary files, ${error}`);
   }
 
-  await cleanUp(paths);
+  try {
+    paths.push(redoLog);
+    await cleanUp(paths);
+  } catch (error) {
+    logger.error(`failed to shut down correctly, ${error}`);
+  }
 }
 
 bootstrap();
