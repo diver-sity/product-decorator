de0365fbcb51653d86b2470dff3f4dbdd36b198d
Lee Zhao <lee.simon3@hotmail.com>
2020-11-22 17:21:03 +1100

    handles incorrect payload format

--- src/services/VideoLinkDecorator.spec.ts
+++ src/services/VideoLinkDecorator.spec.ts
@@ -11,14 +11,25 @@ describe('VideoLinkDecorator', () => {
   });
 
   describe('#addVideoURL', () => {
-    it('fetches video URLs for a given product', () => {
-      get.mockResolvedValue({
+    it('fetches and associates video URLs for a given product', () => {
+      get.mockResolvedValueOnce({
         data: { "_links": { "self": { "href": "https:\/\/eve.theiconic.com.au\/catalog\/products\/cm638aa10yrl\/videos?page=1" } }, "_embedded": { "videos_url": [{ "url": "https:\/\/video_host\/1271455204.mp4", "_links": { "self": { "href": "https:\/\/video_host\/catalog\/products\/cm638aa10yrl\/videos" } } }] }, "page_count": 1, "page_size": 25, "total_items": 1, "page": 1 }
       })
 
       expect.assertions(1);
-      return addVideoURL(socks).then((result: Product) => {
+      return addVideoURL({ ...socks }).then((result: Product) => {
         expect(result.video_urls).toEqual(["https:\/\/video_host\/1271455204.mp4"])
+      })
+    });
+
+    it('handles incorrect payload format', () => {
+      get.mockResolvedValueOnce({
+        data: { "_links": { "self": { "href": "https:\/\/eve.theiconic.com.au\/catalog\/products\/cm638aa10yrl\/videos?page=1" } }, "_embedded": { "something": "else" }, "page_count": 1, "page_size": 25, "total_items": 1, "page": 1 }
+      })
+
+      expect.assertions(1);
+      return addVideoURL({ ...socks }).then((result: Product) => {
+        expect(result.video_urls).toBeUndefined();
       })
     });
 
--- src/services/VideoLinkDecorator.ts
+++ src/services/VideoLinkDecorator.ts
@@ -1,3 +1,4 @@
+import { isEmpty } from "lodash";
 import axios from "../config/axiosInstance";
 import Product from "src/models/Product";
 import VideoPreviewPayload from "src/dto/VideoPreviewPayload";
@@ -8,11 +9,13 @@ const addVideoURL = async (product: Product) => {
     const url = `https://eve.theiconic.com.au/catalog/products/${product.sku}/videos`;
     return axios.get(url).then(response => {
         const payload = response.data as VideoPreviewPayload;
-        const urls = payload._embedded.videos_url.map(e => e.url);
-        product.video_urls = urls;
+        if (payload._embedded && !isEmpty(payload._embedded.videos_url)) {
+            const urls = payload._embedded.videos_url.map(e => e.url);
+            product.video_urls = urls;
+        }
         return product;
     }).catch((error: AxiosError) => {
-        logger.error(`failed to access ${url}, error: ${JSON.stringify(error.response.data)}`);
+        logger.warn(`failed to access ${url}, error: ${JSON.stringify(error.response.data)}`);
         return product;
     });
 };
