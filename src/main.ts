8a630d2bad78904b93775f1b2754948e816abf03
Lee Zhao <lee.simon3@hotmail.com>
2020-11-22 15:11:15 +1100

    fulfilling operations

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import * as fs from "fs";
 import fetchFromSource from "./services/RestAPIExtractor";
 import assembleSmallFiles from './services/InterimFileAssembler';
 import { path } from './services/VideoLinkedProductLoader';
+import logger from "./config/Logger";
+import cleanUp from './services/Cleaner';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
@@ -14,25 +16,24 @@ async function bootstrap() {
       fs.unlinkSync(path);
     }
   } catch (error) {
-    console.error(`failed to clean up before starting, ${JSON.stringify(error)}`);
+    logger.error(`failed to clean up before starting, ${JSON.stringify(error)}`);
     process.exit(2)
   }
 
   let paths;
   try {
-    paths = await fetchFromSource("https://eve.theiconic.com.au/catalog/products?page=1&page_size=1000");
+    paths = await fetchFromSource("https://eve.theiconic.com.au/catalog/products?page=1&page_size=1000&sort=activated_at");
   } catch (error) {
-    console.error(`failed to fetch data, ${JSON.stringify(error)}`);
+    logger.error(`failed to fetch data, ${JSON.stringify(error)}`);
   }
 
-  process.on("exit", async () => {
-    if (paths) {
-      const promises = paths.map(p => fs.unlink(p, () => {
-        console.info(`cleaned up, ${p}`);
-      }))
-      await Promise.all(promises);
-    }
-  })
-  assembleSmallFiles(paths);
+  try {
+    assembleSmallFiles(paths);
+  } catch (error) {
+    logger.error(`failed to assemble temporary files, ${JSON.stringify(error)}`);
+  }
+
+  await cleanUp(paths);
 }
+
 bootstrap();
