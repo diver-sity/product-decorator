3dfdcb5bfcf879035b7f2db700eeed988b284e11
Lee Zhao <lee.simon3@hotmail.com>
2020-11-23 07:53:37 +1100

    minor refactoring

--- src/services/RestAPIExtractor.ts
+++ src/services/RestAPIExtractor.ts
@@ -10,24 +10,24 @@ import ProductFetchError from "../errors/ProductFetchError";
 import FileOperationError from "../errors/FileOperationError";
 import TimeoutError from "../errors/TimeoutError";
 import { redoLog } from "../config/constants";
+import { AxiosResponse } from "axios";
 
 const fetchFromSource = async (firstPageURL: string) => {
-    const remainingPage = [firstPageURL];
+    const remainingURLs = [firstPageURL];
     const paths = [];
 
-    while (remainingPage.length > 0) {
-        const page = remainingPage.pop();
-        logger.info(`fetching ${page}`);
-        let response;
+    while (remainingURLs.length > 0) {
+        const url = remainingURLs.pop();
+        logger.info(`fetching ${url}`);
 
+        let response;
         try {
-            response = await fs.promises.writeFile(redoLog, page, { flag: "w" })
-                .then(() => axios.get(page));
+            response = await fetchPage(url);
         } catch (error) {
             if (error.code === "ECONNABORTED") {
-                throw new TimeoutError(`failed to fetch data around ${remainingPage}`, error);
+                throw new TimeoutError(`failed to fetch data around ${remainingURLs}`, error);
             }
-            throw new ProductFetchError(`failed to fetch data around ${remainingPage}`, error);
+            throw new ProductFetchError(`failed to fetch data around ${remainingURLs}`, error);
         }
 
         const data = response.data as ProductPagePayload;
@@ -39,19 +39,24 @@ const fetchFromSource = async (firstPageURL: string) => {
                 !isEmpty(data._links) && !isEmpty(data._links.next) &&
                 !isEmpty(data._links.next.href)
             ) {
-                remainingPage.push(data._links.next.href);
+                remainingURLs.push(data._links.next.href);
             }
 
             try {
                 await processPage(data, paths);
             } catch (error) {
-                throw new FileOperationError(`failed to save data around ${remainingPage}`, error);
+                throw new FileOperationError(`failed to save data around ${remainingURLs}`, error);
             }
         }
     }
 
     return Promise.resolve(paths);
 };
+
+async function fetchPage(page: string): Promise<AxiosResponse> {
+    return fs.promises.writeFile(redoLog, page, { flag: "w" })
+        .then(() => axios.get(page));
+}
 
 async function processPage(data: ProductPagePayload, paths: Array<string>) {
     const page = data.page;
